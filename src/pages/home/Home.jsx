e24245a19d96ce2ba3b6ed37b78beef6a8795305
Ankit BagheL <ak9664773@gmail.com>
2023-10-06 00:07:23 +0530

    made home responsive

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import img1 from "../../assets/carousel-1.jpg"
-import img2 from "../../assets/carousel-2.jpg"
 import "./style.scss"
 
 function Home() {
@@ -9,7 +7,7 @@ function Home() {
     <div className="hContainer">
     <div className="carousel">
       <div className="hDiv1">
-        <div className="hContent">
+        <div className="hContent ">
           <div className="data">
             <div className="hHeading">No 1 Pest Control Services</div>
             <div className="hDesc">Enjoy Your Home Totally Pest Free</div>
@@ -28,9 +26,14 @@ function Home() {
       </div>
     </div>
     
-      <div className="hLeft">{`<`}</div>
+      <div className="hLeft" >{`<`}</div>
       <div className="hRight">{">"}</div>
-      
+
+      <div className="hDots">
+        <div className="hDot1"/>
+        <div className="hDot2"/>
+      </div>
+
     </div>
     </>
 
