7f97ee96b68c56bbf601c54f700c876c5b3e2ba4
Ankit BagheL <ak9664773@gmail.com>
2023-10-10 19:35:22 +0530

    add contact page with google map

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import Map from './map/Map'
+// import ContactTile from './contactTile/ContactTile'
+
+import "./style.css"
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className="serviceContainer">
+      <div className="serviceBox1">
+        <div className="serviceTitle">Get In Touch</div>
+        <div className="serviceHeading">Contact for any query</div>
+      </div>
+
+      <div className="serviceBox2">
+        <div className="contactContainer">
+          <Map className="mapCompo"/>
+          <div className="contactForm">
+            <div className="contactFormDetails">
+              The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <span className='contactFormSapn'>Download Now</span>.
+            </div>
+            <div className="contactFormPage">
+              <input type="text" className="contactFormPageText" placeholder='Your Name' />
+              <input type="email" className="contactFormPageText" placeholder='Your Email' />
+              <input type="text" className="contactFormPageText" placeholder='Subject' />
+              <textarea name="Message" className="contactFormPageTextArea" ></textarea>
+              <div className="pricingTileButton contactButton">SEND MESSAGE</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="serviceBox3 contactFormSection3" >
+        <ContactTile details={"Follow Us"} />
+        <ContactTile details={"Address"} title={<FaLocationDot className='contactTileTurnLogo'/>}/>
+        <ContactTile details={"Call Us"} title={<BsFillSignTurnRightFill className='contactTileTurnLogo'/>}/>
+        <ContactTile details={"Email Us"} title={<BsFillSignTurnRightFill className='contactTileTurnLogo'/>}/>
+      </div> */}
+    </div>
   )
 }
 
---
+++ src/pages/contact/contactTile/ContactTile.jsx
@@ -0,0 +1,31 @@
+// import React from 'react'
+// import { FaFacebookF } from "react-icons/fa"
+// import { FaTwitter } from "react-icons/fa"
+// import { FaLinkedinIn } from "react-icons/fa"
+// import { FaInstagram } from "react-icons/fa"
+// import {BsFillSignTurnRightFill} from "react-icons/bs"
+// import {FaLocationDot} from "react-icons/fa"
+
+
+// function ContactTile({details,title}) {
+//     return (
+//         <div className="contactTileContainer">
+//             <div className="contactTileBox">
+//                 <div className="contactTileLogo">{<BsFillSignTurnRightFill className='contactTileTurnLogo'/>}</div>
+//                 <div className="contactTileSection">
+//                     <div className="contactTileDetails">{title}</div>
+//                     <div className="contactTileSocials">
+//                         <div className="footerBox footerSocials">
+//                             <FaFacebookF className='footerSocial' />
+//                             <FaTwitter className='footerSocial' />
+//                             <FaLinkedinIn className='footerSocial' />
+//                             <FaInstagram className='footerSocial' />
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default ContactTile
